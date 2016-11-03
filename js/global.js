07f82fcf702c6911e26746a70d1d82e27f22c0db
kwhuff <kwhuff@gmail.com>
2016-11-03 16:16:49 -0400

    Added stuff

---
+++ js/global.js
@@ -0,0 +1,29 @@
+var signup = document.getElementById('signup')
+signup.addEventListener('click', sendSignupData)
+
+function sendSignupData(){
+  var name = document.querySelector('#name').value
+  var email = document.querySelector('#email').value
+  var password = document.querySelector('#password').value
+  var file = document.querySelector('#file').value
+  // console.log(name, email, password, file)
+
+  fetch('http://38115110.ngrok.io/api/signup', {
+    body: JSON.stringify({
+      name: name,
+      email: email,
+      password: password,
+      file: file,
+    }),
+    method: 'POST',
+    headers:{
+      'Content-Type': 'application/json'
+    }
+  })
+  .then(response => response.json())
+  .then(response => console.log(response))
+  // .then(signedupRedirect)
+}
+  function signedupRedirect(){
+
+  }
