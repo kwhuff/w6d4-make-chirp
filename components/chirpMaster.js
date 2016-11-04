80b41bc7d4ccba9e8e8b5eab3baca66136c91103
kwhuff <kwhuff@gmail.com>
2016-11-04 12:03:55 -0400

    collin magic.

--- components/chirpMaster.js
+++ components/chirpMaster.js
@@ -1,25 +1,20 @@
-// import React from 'react'
-// import ReactDOM from 'react-dom'
-//
-// import MyProfile from '../components/MyProfile'
-// import Following from '../components/Following'
-// import Followers from '../components/Followers'
-// import AllChirps from '../components/AllChirps'
+import React from 'react'
+import ReactDOM from 'react-dom'
 
-// window.renderView = function() {
-// ReactDOM.render(
-//   <div>
-//     <MyProfile />
-//     <Following />
-//     <Followers />
-//     <AllChirps />
-//   </div>,
-//   document.querySelector('#mainScreen')
-// )
+import MyProfile from '../components/MyProfile'
+import Following from '../components/Following'
+import Followers from '../components/Followers'
+import AllChirps from '../components/AllChirps'
+
+window.renderView = function() {
+ReactDOM.render(
+  <div>
+    <AllChirps data={data} />
+  </div>,
+  document.querySelector('#mainScreen')
+)
 function renderView(){
-// fetch('http://38115110.ngrok.io/api/all')
-.then(response => response.json())
-.then(response => console.log(response))
+
 }
 
 renderView()
--- js/signup.js
+++ js/signup.js
@@ -1,48 +1,16 @@
 var signup = document.getElementById('signup')
 signup.addEventListener('click', sendSignupData)
 
-// function sendSignupData(){
-  // var name = document.querySelector('#name').value
-  // var email = document.querySelector('#email').value
-  // var password = document.querySelector('#password').value
-  // var file = document.querySelector('#file')
-  // console.log(name, email, password, file)
-//
-  // fetch('http://38115110.ngrok.io/api/signup', {
-  //   body: JSON.stringify({
-  //     name: name,
-  //     email: email,
-  //     password: password,
-  //     file: file,
-  //   }),
-  //   method: 'POST',
-  //   headers:{
-  //     'Content-Type': 'application/json'
-  //   }
-  // })
-//   .then(response => response.json())
-//   .then(response => console.log(response))
-//   .then(signedupRedirect)
-//
-// }
-// function signedupRedirect(response){
-//       sessionStorage.setItem('chirp', response.user.api_token)
-//       window.location.href = '/postLogin.html'
-//     }
-
 function sendSignupData(){
-  var name = document.querySelector('#name').value
-  var email = document.querySelector('#email').value
+  var formData = new FormData()
+
+   formData.name = document.querySelector('#name').value
+  formData.email = document.querySelector('#email').value
   var password = document.querySelector('#password').value
-  var file = document.querySelector('#file')
+  var file = document.querySelector('#file').files[0]
 
   fetch('http://38115110.ngrok.io/api/signup', {
-    body: JSON.stringify({
-      name: name,
-      email: email,
-      password: password,
-      file: file,
-    }),
+    body: formData,
     method: 'POST',
     headers:{
       'Content-Type': 'application/json'
