a5ad3d5f91e02da6f03e2846b5300c029ca3e92b
kwhuff <kwhuff@gmail.com>
2016-11-04 11:26:33 -0400

    small changes

--- components/chirpMaster.js
+++ components/chirpMaster.js
@@ -1,21 +1,25 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
+// import React from 'react'
+// import ReactDOM from 'react-dom'
+//
+// import MyProfile from '../components/MyProfile'
+// import Following from '../components/Following'
+// import Followers from '../components/Followers'
+// import AllChirps from '../components/AllChirps'
 
-import MyProfile from '../components/MyProfile'
-import Following from '../components/Following'
-import Followers from '../components/Followers'
-import AllChirps from '../components/AllChirps'
-
-window.renderView = function() {
-ReactDOM.render(
-  <div>
-    <MyProfile />
-    <Following />
-    <Followers />
-    <AllChirps />
-  </div>,
-  document.querySelector('#mainScreen')
-)
+// window.renderView = function() {
+// ReactDOM.render(
+//   <div>
+//     <MyProfile />
+//     <Following />
+//     <Followers />
+//     <AllChirps />
+//   </div>,
+//   document.querySelector('#mainScreen')
+// )
+function renderView(){
+// fetch('http://38115110.ngrok.io/api/all')
+.then(response => response.json())
+.then(response => console.log(response))
 }
 
 renderView()
--- js/signup.js
+++ js/signup.js
@@ -1,17 +1,40 @@
-function signedupRedirect(data){
-  sessionStorage.setItem('chirp', data.user.api_token)
-  window.location.href = '/postLogin.html'
-}
-
 var signup = document.getElementById('signup')
 signup.addEventListener('click', sendSignupData)
+
+// function sendSignupData(){
+  // var name = document.querySelector('#name').value
+  // var email = document.querySelector('#email').value
+  // var password = document.querySelector('#password').value
+  // var file = document.querySelector('#file')
+  // console.log(name, email, password, file)
+//
+  // fetch('http://38115110.ngrok.io/api/signup', {
+  //   body: JSON.stringify({
+  //     name: name,
+  //     email: email,
+  //     password: password,
+  //     file: file,
+  //   }),
+  //   method: 'POST',
+  //   headers:{
+  //     'Content-Type': 'application/json'
+  //   }
+  // })
+//   .then(response => response.json())
+//   .then(response => console.log(response))
+//   .then(signedupRedirect)
+//
+// }
+// function signedupRedirect(response){
+//       sessionStorage.setItem('chirp', response.user.api_token)
+//       window.location.href = '/postLogin.html'
+//     }
 
 function sendSignupData(){
   var name = document.querySelector('#name').value
   var email = document.querySelector('#email').value
   var password = document.querySelector('#password').value
   var file = document.querySelector('#file')
-  // console.log(name, email, password, file)
 
   fetch('http://38115110.ngrok.io/api/signup', {
     body: JSON.stringify({
@@ -26,6 +49,22 @@ function sendSignupData(){
     }
   })
   .then(response => response.json())
-  .then(response => console.log(response))
-  .then(signedupRedirect)
+  .then(signedupHandler)
+
+}
+
+function signedupHandler(response){
+  if ( typeof response.user != 'undefined'){
+    sessionStorage.setItem('chirp', response.user.api_token)
+    window.location.href = '/postLogin.html'
+  }
+  else{
+    response.forEach(function(error){
+
+      var errorDiv = document.createElement('div')
+      errorDiv.classList.add('alert', 'alert-danger')
+      errorDiv.innerHTML = error
+      document.querySelector('#errors').appendChild(errorDiv)
+    })
+  }
 }
