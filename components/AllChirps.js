71c07b566eeac775e23050d0eacbac9f94670a01
kwhuff <kwhuff@gmail.com>
2016-11-04 13:48:01 -0400

    working on components

--- components/AllChirps.js
+++ components/AllChirps.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link } from 'react-router'
 
 class AllChirps extends React.Component{
   constructor(props){
@@ -6,29 +7,36 @@ class AllChirps extends React.Component{
     this.updateChirps = this.updateChirps.bind(this)
 
     this.state = {
-      chirps: []
+      chirp: '',
+      name: '',
+      file: ''
     }
   }
 
   componentDidMount(){
-    fetch('http://38115110.ngrok.io/api/all')
+    // fetch('http://38115110.ngrok.io/api/all')
     .then(response => response.json())
+    .then(response => console.log(response))
     .then(this.updateChirps)
   }
 
   updateChirps(response) {
-    console.log(response)
+    // console.log(response)
     this.setState({
-      chirps: response
+      chirp: response.user.chirp,
+      name: response.user.name,
+      file: response.user.file
     })
   }
 
-  render(){//run your map here
+  render(){
     var chirps = this.state.chirps.map((chirp, i) => {
       return <Chirp chirp={chirp} />
     })
     return <div>
+      <h1>helloooooooo</h1>
       {chirps}
     </div>
   }
 }
+export default AllChirps
---
+++ js/global.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { Router, Route, Link, browserHistory } from 'react-router'
+
+import AllChirps from '../components/AllChirps'
+import MyChirps from '../components/MyChirps'
+import Followers from '../components/Followers'
+import Following from '../components/Following'
+
+ReactDOM.render(
+<Router history={browserHistory}>
+  <Route path="/" component={AllChirps}/>
+  <Route path="/mychirps" component={MyChirps}/>
+  <Route path="/followers" component={Followers}/>
+  <Route path="/following" component={Following}/>
+</Router>,
+  document.querySelector('#mainScreen')
+)
